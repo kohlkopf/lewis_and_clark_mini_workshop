2d4ef597bc6eb90c8c9d05fcbbe5c2ed635ef77e
kohlkopf <kohlkinning@gmail.com>
2018-11-14 16:43:28 +0000

    add more file structure, bring in unix tutorial

---
+++ unix_tutorial/documents/unix_nav.sh
@@ -0,0 +1,30 @@
+pwd
+ls
+cd data
+ls
+ls -F
+cd untrimmed_fastq
+ls -F
+cd /cloud/project/lewis_and_clark_mini_workshop/unix_tutorial
+cd data
+cd untrimmed_fastq
+cd /cloud/project/lewis_and_clark_mini_workshop/unix_tutorial
+cd data
+cd untrimmed_fastq
+ls SR*
+ls SRR09*
+
+head -n4 SRR098026.fastq
+cd /cloud/project/lewis_and_clark_mini_workshop/unix_tutorial/data/untrimmed_fastq/
+/cloud/project/lewis_and_clark_mini_workshop/unix_tutorial/software/FastQC/fastqc *.fastq
+mkdir /cloud/project/lewis_and_clark_mini_workshop/unix_tutorial/results
+mkdir /cloud/project/lewis_and_clark_mini_workshop/unix_tutorial/results/fastqc_untrimmed_reads
+mv *.zip /cloud/project/lewis_and_clark_mini_workshop/unix_tutorial/results/fastqc_untrimmed_reads/
+mv *.html /cloud/project/lewis_and_clark_mini_workshop/unix_tutorial/results/fastqc_untrimmed_reads/
+cd /cloud/project/lewis_and_clark_mini_workshop/unix_tutorial/results/fastqc_untrimmed_reads/
+ls
+unzip '*.zip'
+ls
+ls -F
+ls -F SRR097977_fastqc/
+less SRR097977_fastqc/summary.txt
